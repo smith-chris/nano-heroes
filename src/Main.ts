452ada480b68f212c196040bf4d7c126eaa9e13b
smith-chris <kowalski.k@outlook.com>
2018-02-17 22:47:40 +0000

    Added very simple map and character.

--- src/Main.ts
+++ src/Main.ts
@@ -1,23 +1,42 @@
 import { stage, screen, ticker } from 'app/app'
-import { Sprite } from 'pixi.js'
-import bunnyImage from 'assets/bunny.png'
+import { Sprite, Container } from 'pixi.js'
+import char1 from 'assets/char1.png'
+import grassImage from 'assets/grass.png'
 
 // create a new Sprite from an image path
-const bunny = Sprite.fromImage(bunnyImage.src)
+const character = Sprite.fromImage(char1.src)
+
+const map = new Container()
+
+map.x = 2
+map.y = 2
+
+for (let x = 0; x < 5; x++) {
+  for (let y = 0; y < 30; y++) {
+    const grass = Sprite.fromImage(grassImage.src)
+    const isOdd = y % 2 === 1
+    grass.x = x * 24
+    if (isOdd) {
+      grass.x += 12
+    }
+    grass.y = y * 4
+    map.addChild(grass)
+  }
+}
 
 // center the sprite's anchor point
-bunny.anchor.set(0.5)
+character.anchor.set(0.5)
 
 // move the sprite to the center of the screen
-bunny.x = screen.width / 2
-bunny.y = screen.height / 2
+character.x = screen.width / 2
+character.y = screen.height / 2
 
-stage.addChild(bunny)
+stage.addChild(map)
+stage.addChild(character)
 
 // Listen for animate update
 ticker.add((delta: number) => {
   // just for fun, let's rotate mr rabbit a little
   // delta is 1 if running at 100% performance
   // creates frame-independent transformation
-  bunny.rotation += 0.03 * delta
 })
