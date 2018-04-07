a3323b50d249207eb3a235affc792839e57ae6fb
smith-chris <kowalski.k@outlook.com>
2018-04-07 09:13:57 +0100

    Creatures automatically fight on game initialization for debugging purposes.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,14 +5,17 @@ import Creatures from 'components/Creatures'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { battleActions } from 'store/battle'
+import { uiActions } from 'store/ui'
 import { Creature } from 'transforms/creature'
 import { Point } from 'utils/pixi'
+import { getHex } from 'transforms/map/map'
+import { store } from 'store/store'
 
 const mapStateToProps = (state: StoreState) => state
 type StateProps = ReturnType<typeof mapStateToProps>
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
-  return bindActionCreators(battleActions, dispatch)
+  return bindActionCreators({ ...battleActions, ...uiActions }, dispatch)
 }
 type DispatchProps = ReturnType<typeof mapDispatchToProps>
 
@@ -29,6 +32,8 @@ class Game extends Component<Props> {
       battle,
       moveSelected,
       changeTurn,
+      highlightTarget,
+      resetPositions,
     } = this.props
     loadMap({ width: 9, height: 10 })
     addAttackers([2, 5, 8].map(y => new Creature(new Point(0, y))))
@@ -38,6 +43,15 @@ class Game extends Component<Props> {
     setTimeout(() => {
       moveSelected(new Point(4, 5))
     }, 500)
+    setTimeout(() => {
+      const state = store.getState()
+      const hex = getHex(state.battle.hexes, new Point(4, 5))
+      highlightTarget({ battle: state.battle, hex })
+    }, 1500)
+    setTimeout(() => {
+      moveSelected(new Point(5, 4))
+      resetPositions()
+    }, 2000)
   }
   render() {
     return (
--- src/transforms/creature/Creature.ts
+++ src/transforms/creature/Creature.ts
@@ -7,7 +7,7 @@ import { idGenerator } from 'utils/math'
 const randomGenerator = new RandomGenerator()
 
 const pixieModel = {
-  attack: 2,
+  attack: 40, // TODO: set value back to 2
   defence: 2,
   damage: {
     min: 1,
